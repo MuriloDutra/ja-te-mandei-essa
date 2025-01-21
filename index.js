f010c09bb8e380047d55a85b20a40181389d85b0
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2025-01-21 19:54:15 -0300

    New animation: move-letter-upfront

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const openLetterClasName = 'open-letter'
-const closeLetterClasName = 'close-letter'
-let main = document.querySelector('main');
-let envelope = document.querySelector('.envelope');
+const openLetterClasName = "open-letter";
+const closeLetterClasName = "close-letter";
+let main = document.querySelector("main");
+let envelope = document.querySelector(".envelope");
 
-envelope.addEventListener('click', () => {
-    if(main.classList.contains('open-letter')){
-        main.classList.add(closeLetterClasName);
-        main.classList.remove(openLetterClasName);
-    }else{
-        main.classList.add(openLetterClasName);
-        main.classList.remove(closeLetterClasName);
-    }
+envelope.addEventListener("click", () => {
+  if (main.classList.contains("open-letter")) {
+    main.classList.add(closeLetterClasName);
+    main.classList.remove(openLetterClasName);
+  } else {
+    main.classList.add(openLetterClasName);
+    main.classList.remove(closeLetterClasName);
+  }
 });
