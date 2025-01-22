9d875a3ad93646b6118834067dd972a2f44b4c0d
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2025-01-21 21:00:23 -0300

    Added placeholder text

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const openLetterClasName = "open-letter";
-const closeLetterClasName = "close-letter";
-let main = document.querySelector("main");
-let envelope = document.querySelector(".envelope");
+function handleEnvelopeClick() {
+  const openLetterClasName = "open-letter";
+  const closeLetterClasName = "close-letter";
+  let main = document.querySelector("main");
 
-envelope.addEventListener("click", () => {
   if (main.classList.contains("open-letter")) {
     main.classList.add(closeLetterClasName);
     main.classList.remove(openLetterClasName);
@@ -11,4 +10,8 @@ envelope.addEventListener("click", () => {
     main.classList.add(openLetterClasName);
     main.classList.remove(closeLetterClasName);
   }
-});
+}
+
+function handleLetterClick(event) {
+  event.stopPropagation();
+}
