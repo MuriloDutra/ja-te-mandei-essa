c2a6a746aadcda7bd3cf24aa84fd2218ee8f514f
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2025-01-22 20:59:05 -0300

    song engine - DONE

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
+const songs = [
+  {
+    name: "ei, coisinha!",
+    artist: "Clara Valverde",
+    image: "assets/ei-coisinha.jpeg",
+  },
+  {
+    name: "Alegre Menina",
+    artist: "Dori Caymmi",
+    image: "assets/alegre-menina.jpg",
+  },
+  {
+    name: "Mutante",
+    artist: "Rita Lee",
+    image: "assets/mutante.jpg",
+  },
+  {
+    name: "Tatuagem",
+    artist: "Chico Buarque",
+    image: "assets/tatuagem.jpg",
+  },
+  {
+    name: "burburinho",
+    artist: "Clara Valverde",
+    image: "assets/ei-coisinha.jpeg",
+  },
+  {
+    name: "Baby 95",
+    artist: "Liniker",
+    image: "assets/baby95.jpg",
+  },
+  {
+    name: "Muito",
+    artist: "Caetano Veloso",
+    image: "assets/muito.jpg",
+  },
+  {
+    name: "La Belle De Jour",
+    artist: "Alceu Valença",
+    image: "assets/la-belle-de-jour.jpg",
+  },
+  {
+    name: "Sick Love",
+    artist: "Red Hot Chili Peppers",
+    image: "assets/sick-love.jpg",
+  },
+];
+
 function handleEnvelopeClick() {
   const openLetterClasName = "open-letter";
   const closeLetterClasName = "close-letter";
@@ -16,7 +64,27 @@ function handleLetterClick(event) {
   event.stopPropagation();
 }
 
-function showMusic() {
-  let musicContainer = document.querySelector(".music-container");
-  musicContainer.classList.toggle("show-music-card");
+function toggleSongCard(song) {
+  const match = songs.find(({ name }) => name == song);
+
+  const songName = document.getElementById("song-name");
+  const artist = document.getElementById("artist");
+  const songImage = document.getElementById("song-image");
+
+  songName.textContent = match.name;
+  artist.textContent = match.artist;
+  songImage.src = match.image;
+  songImage.alt = `${match.artist} | ${match.name}`;
+
+  const showMusicClassName = "show-music-card";
+  const hideMusicClassName = "hide-music-card";
+  const musicContainer = document.querySelector(".music-container");
+
+  if (musicContainer.classList.contains(showMusicClassName)) {
+    musicContainer.classList.add(hideMusicClassName);
+    musicContainer.classList.remove(showMusicClassName);
+  } else {
+    musicContainer.classList.add(showMusicClassName);
+    musicContainer.classList.remove(hideMusicClassName);
+  }
 }
